5088790ecf8ed912ac09aad2fc45eaf7f450dc01
mea <shennaconde31@gmail.com>
2025-04-26 12:52:33 +0800

    una una
    una

---
+++ iptweb2_final-main/nextjs-app/my-app/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p className="text-lg">This is a simple Next.js app with Tailwind CSS.</p>
+    </div>
+    
+    
+  );
+}
